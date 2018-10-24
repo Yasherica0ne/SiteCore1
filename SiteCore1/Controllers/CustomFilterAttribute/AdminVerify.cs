dfc8755ded1dfe14796cd526961aac6eff345bb2
Yasherica0ne <viktor.makarov87@mail.ru>
2018-10-24 17:55:50 +0300

    Admin authentification added

---
+++ SiteCore1/Controllers/CustomFilterAttribute/AdminVerify.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SiteCore1.Controllers.CustomRouteAttribute
+{
+    public class AdminVerify : ActionFilterAttribute
+    {
+        private const string secretKey = "sitecore";
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var key = filterContext.ActionParameters["secret"];
+            if (key == null || !secretKey.Equals(key))
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+            }
+        }
+    }
+}
